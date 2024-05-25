6ecb180cea7ee28bf989a2647cba4f2566255718
Maiko <38130439+Maikoeliud@users.noreply.github.com>
2024-05-25 20:43:30 +0300

    Delete assignment4/target directory

--- assignment4/target/debug/deps/assignment4-eb0099f0c6d71382.d
+++
@@ -1,9 +0,0 @@
-/home/maiko/Desktop/Rust/rust-class/class-assignments/assignment4/target/debug/deps/libassignment4-eb0099f0c6d71382.rmeta: src/main.rs src/countries/mod.rs src/countries/african_countries.rs src/countries/asian_countries.rs src/Fruits.rs
-
-/home/maiko/Desktop/Rust/rust-class/class-assignments/assignment4/target/debug/deps/assignment4-eb0099f0c6d71382.d: src/main.rs src/countries/mod.rs src/countries/african_countries.rs src/countries/asian_countries.rs src/Fruits.rs
-
-src/main.rs:
-src/countries/mod.rs:
-src/countries/african_countries.rs:
-src/countries/asian_countries.rs:
-src/Fruits.rs:
